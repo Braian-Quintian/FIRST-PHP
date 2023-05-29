486573ab1114c989527b71cf7ad0fbef08dc5809
Braian-Quintian <bquintianromero@gmail.com>
2023-05-29 16:56:01 -0500

    feat: :sparkles: Que es PHP?
    Se coloca un breve resumen de que es php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    
+    /**
+     * ! 1. (5) INTRODUCCIÓN PHP
+     * *Que es PHP?
+     * 
+     * PHP es un lenguaje de programación para crear aplicaciones 
+     * web dinámicas en el servidor.
+     * Permite generar contenido personalizado en
+     * tiempo real y se integra fácilmente con HTML.
+     * Es ampliamente utilizado debido a su 
+     * facilidad de uso y compatibilidad con diferentes
+     * sistemas operativos y servidores web.
+     */
 ?>
