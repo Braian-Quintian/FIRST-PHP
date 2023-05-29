d01724d1cf31532916ead1e347cb4d7fa1e2c916
Braian-Quintian <bquintianromero@gmail.com>
2023-05-29 17:07:48 -0500

    feat: :sparkles: Estructuración del proyecto PHP
    Como se tiene que estructurar el proyecto php, incluyendo para que sirve cada carpeta

--- index.php
+++ index.php
@@ -10,5 +10,27 @@
      * Es ampliamente utilizado debido a su 
      * facilidad de uso y compatibilidad con diferentes
      * sistemas operativos y servidores web.
+     * 
+     * !! 1.1. (5) ESTRUCTURA BÁSICA DE UN PROYECTO PHP
+     * ?Es importante tener en cuenta que los proyectos PHP !REQUIEREN¡
+     * ??ser almacenados en el documentRoot del servidor web.
+     * *El documentRoot es la carpeta principal del servidor Apache.
+     * @example: /var/www/html (Linux)
+     * @example: C:\xampp\htdocs (Windows)
+     * 
+     * !!ESTRUCTURA BÁSICA DE UN PROYECTO WEB/PHP
+     * *CSS
+     * ? ESTILOS (Cascading Style Sheets)
+     * *JS
+     * ? CÓDIGO JAVASCRIPT (JavaScript)
+     * *IMG
+     * ? IMÁGENES (Images)
+     * *SCRIPTS
+     * ? CÓDIGO PHP (PHP)
+     * *UPLOADS
+     * ?(Archivos subidos por los usuarios)
+     * *INDEX.PHP (Página principal)
+     * ?(Página principal del proyecto)
+     * 
      */
 ?>
