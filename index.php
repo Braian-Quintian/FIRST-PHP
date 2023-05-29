1237c98c8e7e4d74d908d8a6b16abe19678080c3
Braian-Quintian <bquintianromero@gmail.com>
2023-05-29 17:29:44 -0500

    feat: :sparkles: Tipos de datos
    Tipos de datos

--- index.php
+++ index.php
@@ -35,7 +35,31 @@
      * !? UN ARCHIVO PHP CONTIENE LA SIGUIENTE SINTAXIS
      * ?             <?php ..(código).. ?>
      * 
-     * !? DATOS
+     * ! 2. (6) FUNCIONES DE SALIDA
+     * *echo
+     * Imprime uno o más valores separados por comas.
+     * ?echo "Hola Mundo";
+     * 
+     * *printf
+     * Imprime un texto con formato.
+     * ??$texto = "Mundo";
+     * ?printf("Hola %s", $texto);'
+     * !!!! %s es un marcador de posición para una cadena de texto. 
+     * @return string printf "Hola Mundo"
+     * 
+     * *sprintf
+     * Devuelve un texto con formato.
+     * ??$texto = "Mundo";
+     * ?$saludo = sprintf("Hola %s", $texto);
+     * !!!! solamente devuelve el dato más no lo imprime.
+     * 
+     * *var_dump
+     * Imprime el tipo de dato y el valor de una variable.
+     * ?$texto = "Mundo";
+     * ?var_dump($texto);
+     * @return mixed var_dump "string(5) "Mundo""
+     * 
+     *  ! 3. (7) VARIABLES Y CONSTANTES
      * *Variables 
      * se definen como un signo $ antes del nombre de la variable.
      * ?$nombre = "Juan";
@@ -43,6 +67,43 @@
      * *Constantes
      * se definen con la función define().
      * ?define("PI", 3.1416);
+     * 
+     * !! 3.1. (7) TIPOS DE DATOS
+     * 
+     * !!! |°J°| SIEMPRE COLOCAR EL TIPO DE DATO DE LA VARIABLE.
+     * *Enteros
+     * Son números enteros sin decimales.
+     * ?$numero = 200;
+     * 
+     * *Flotantes
+     * Son números con decimales.
+     * ?$numero = 200.5;
+     * 
+     * *Cadenas de texto
+     * Son cadenas de texto.
+     * ?$texto = "Hola Mundo";
+     * 
+     * *Booleanos
+     * Son valores de verdadero o falso.
+     * ?$verdadero = true;
+     * ?$falso = false;
+     * 
+     * *Nulos
+     * Son valores nulos.
+     * ?$nulo = null;
+     * 
+     * *Arrays
+     * Son arreglos de datos.
+     * ?$array = [];
+     * 
+     * *Objetos
+     * Son instancias de una clase.
+     * ?$objeto = new stdClass();
+     * 
+     * *Recursos
+     * Son referencias a recursos externos.
+     * ?$archivo = fopen("archivo.txt", "r");
+     * @method fopen funciona para abrir un archivo.
      * 
      */
 ?>
